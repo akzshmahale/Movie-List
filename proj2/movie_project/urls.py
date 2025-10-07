d70ec9b1fb23e71eb75365f8a78c6b17580faa3e
akzshmahale <akash24mahale@gmail.com>
2025-10-07 19:40:13 +0530

    Add files via upload

---
+++ proj2/movie_project/urls.py
@@ -0,0 +1,15 @@
+# movie_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings               # Import settings
+from django.conf.urls.static import static     # Import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('movies/', include('movies.urls')),
+]
+
+# Add this line to serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
